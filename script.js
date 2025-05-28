44ec347c186260f04d7ec065fc773635c0f8cdc4
ashim-sharma-console <ashimsharma49@gmail.com>
2025-05-28 11:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-//complete this code
-class Animal {}
+ class Animal {
+      constructor(species) {
+        this._species = species;
+      }
 
-class Dog extends Animal {}
+      get species() {
+        return this._species;
+      }
 
-class Cat extends Animal {}
+      makeSound() {
+        log(`The ${this.species} makes a sound`);
+      }
+    }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+    class Dog extends Animal {
+      bark() {
+        log("woof");
+      }
+    }
+
+    class Cat extends Animal {
+      purr() {
+        log("purr");
+      }
+    }
+
+    // Attach to window (as required)
+    window.Animal = Animal;
+    window.Dog = Dog;
+    window.Cat = Cat;
+
+    // Logging helper
+    function log(message) {
+      const output = document.getElementById('logOutput');
+      output.textContent += message + '\n';
+    }
+
+    // Test functions
+    function testCat() {
+      const myCat = new Cat("Siamese");
+      myCat.makeSound();  // The Siamese makes a sound
+      myCat.purr();       // purr
+    }
+
+    function testDog() {
+      const myDog = new Dog("Golden Retriever");
+      myDog.makeSound();  // The Golden Retriever makes a sound
+      myDog.bark();       // woof
+    }
